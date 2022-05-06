4f373676896676a460863e69d84ca295cc597ee3
Oldman <wyongzhi@outlook.com>
2022-05-06 10:10:54 +0800

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,16 +13,13 @@ module.exports = {
   },
   plugins: [
     '@typescript-eslint',
+    'import', 'mocha',
   ],
   rules: {
     semi: [2, 'never'], // 不要分号
     'no-param-reassign': 0, // 允许对函数参数再赋值
   },
   settings: {
-    'import/resolver': {
-      node: {
-        paths: ['src/'],
-      },
-    },
+
   },
 }
